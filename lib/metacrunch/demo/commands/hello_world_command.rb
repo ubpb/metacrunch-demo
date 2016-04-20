c4f7e181281d86bdc33195ffb821f61d5179d193
René Sprotte <rene.sprotte@provideal.net>
2016-04-20 15:41:25 +0200

    Update for metacrunch 3.x examples.

--- lib/metacrunch/demo/commands/hello_world_command.rb
+++
@@ -1,9 +0,0 @@
-module Metacrunch
-  module Demo
-    class HelloWorldCommand < Metacrunch::Command
-      def perform
-        shell.say "Hello World"
-      end
-    end
-  end
-end
--- lib/metacrunch/demo/version.rb
+++
@@ -1,5 +0,0 @@
-module Metacrunch
-  module Demo
-    VERSION = "1.0.0"
-  end
-end
